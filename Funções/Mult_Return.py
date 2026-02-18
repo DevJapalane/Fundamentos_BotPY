d01993540ca025945441824719803b609745c64e
DevJapalane <kalane.ip.maciel@gmail.com>
2026-02-18 13:23:22 -0300

    Fixed Mult_Return.py

--- Funções/Mult_Return.py
+++ Funções/Mult_Return.py
@@ -8,10 +8,14 @@
 
 # def analisar_mensagem(msg):
 #     print msg.len
-
+msg=input("digite: ")
 def analisar_mensagem(msg):
     qtd_char = len(msg)
-    qtd_wrds = len(msg.split) # .split faz todos espaço na frase virar uma virgula para a lista python, sendo assim 2 palavras separadas por um espaço vira dois itens na lista
+    qtd_wrds = len(msg.split()) # .split faz todos espaço na frase virar uma virgula para a lista python, sendo assim 2 palavras separadas por um espaço vira dois itens na lista
     haslink = "https" in msg
 
-    print(f"a mensagem cont~em {qtd_char} caracteres.")
+    print(f"a mensagem contém {qtd_char} caracteres.")
+    print(f"a mensagem contém {qtd_wrds} palavras.")
+    print(f"contém link? {haslink}")
+
+analisar_mensagem(msg)
